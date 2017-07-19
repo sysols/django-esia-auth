c94e2ea86bffcd2b15d67fe4a804bb23c2a5248e
wolfstein9119 <wolfstein9119@gmail.com>
2017-07-19 11:48:25 +0700

    Prepare for package

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+import russian_fields
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='django-russian-fields',
+    version=russian_fields.__version__,
+    packages=['esia_auth'],
+    install_requires=['django'],
+    include_package_data=True,
+    license='BSD 2-Clause License',
+    description='A simple Django app to use ESIA authentications.',
+    long_description=README,
+    url='https://github.com/wolfstein9119/django-esia-auth',
+    author='Alexey Tatarinov',
+    author_email='wolfstein9119@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD 2-Clause License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
