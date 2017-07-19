96243fbf8ff5ceb67df102fb9a3689b492c99633
wolfstein9119 <wolfstein9119@gmail.com>
2017-07-19 14:32:35 +0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup, find_packages
-import russian_fields
+import esia_auth
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-esia-auth',
-    version=russian_fields.__version__,
+    version=esia_auth.__version__,
     packages=['esia_auth'],
     install_requires=['django'],
     include_package_data=True,
