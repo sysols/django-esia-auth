8311585f1942ba37588a823932af7a3fdf2b0f9e
a.tatarinov <a.tatarinov@sysolutions.ru>
2017-11-13 11:27:38 +0700

    fix locale

--- esia_auth/__init__.py
+++ esia_auth/__init__.py
@@ -1,3 +1,4 @@
 default_app_config = 'esia_auth.apps.AuthConfig'
 
-__version__ = '1.0.0a1'
+__version__ = '1.0.1'
+
--- esia_auth/apps.py
+++ esia_auth/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class AuthConfig(AppConfig):
     name = 'esia_auth'
     verbose_name = _('Esia META')
+
